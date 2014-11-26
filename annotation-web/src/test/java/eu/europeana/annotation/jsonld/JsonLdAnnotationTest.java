b60e6aa646e5a7dfbda9c2d7c4fc3c9f15559a48
GrafR <GrafR@N3SIM1301.D03.arc.local>
2014-11-26 14:07:30 +0100

    gson serialization deserialization initial tests

--- annotation-web/src/test/java/eu/europeana/annotation/jsonld/JsonLdAnnotationTest.java
+++ annotation-web/src/test/java/eu/europeana/annotation/jsonld/JsonLdAnnotationTest.java
@@ -23,8 +23,6 @@ import org.junit.Test;
 
 import com.google.gson.Gson;
 
-import eu.europeana.annotation.solr.model.internal.SolrAnnotationImpl;
-
 /**
  *
  */
@@ -39,24 +37,7 @@ public class JsonLdAnnotationTest {
     }
 
     @Test
-    public void testGsonDeserializationFromJsonLd() {
-    	
-        String jsonLd = "{\"@context\":{\"oa\":\"http://www.w3.org/ns/oa-context-20130208.json\"},\"@type\":\"oa:Annotation\",\"annotatedAt\":\"2012-11-10T09:08:07\",\"annotatedBy\":{\"@type\":\"http://xmlns.com/foaf/0.1/person\",\"name\":\"annonymous web user\"},\"hasBody\":[{\"@type\":\"[oa:Tag, cnt:ContentAsText, dctypes:Text]\",\"chars\":\"Vlad Tepes\",\"dc:language\":\"ro\",\"format\":\"text/plain\"},{\"@type\":\"[oa:SemanticTag]\",\"foaf:page\":\"https://www.freebase.com/m/035br4\"}],\"hasTarget\":{\"@type\":\"[oa:SpecificResource]\",\"hasSelector\":{\"@type\":\"\"},\"hasSource\":{\"@id\":\"http://europeana.eu/portal/record//15502/GG_8285.html\",\"@type\":\"dctypes:Text\",\"format\":\"text/html\"}},\"motivatedBy\":\"oa:tagging\",\"serializedAt\":\"2012-11-10T09:08:07\",\"serializedBy\":{\"@type\":\"prov:SoftwareAgent\",\"foaf:homepage\":\"http://annotorious.github.io/\",\"name\":\"Annotorious\"},\"styledBy\":{\"@type\":\"oa:CssStyle\",\"hasSource\":\"http://annotorious.github.io/latest/themes/dark/annotorious-dark.css\",\"styleClass\":\"annotorious-popup\"}}";
-        Gson gson = new Gson();
-        JsonLd jsonLdObject = gson.fromJson(jsonLd, JsonLd.class);
-        String actual = jsonLdObject.toString();
-        toConsole(actual);
-
-        assertEquals(jsonLd, actual);        
-    }
-    
-    @Test
     public void testAnnotationToJsonLd() {
-    	
-    	SolrAnnotationImpl annotation = new SolrAnnotationImpl();
-		annotation.setAnnotationIdString("/testCollection/TestObject/1");
-		annotation.setLanguage("EN");
-		annotation.setLabel("test annotation");
     	
         JsonLd jsonLd = new JsonLd();
         jsonLd.setUseTypeCoercion(false);
@@ -160,6 +141,117 @@ public class JsonLdAnnotationTest {
         assertEquals(expectedIndent, actualIndent);
     }
     
+    @Test
+    public void testGsonSerializationForJsonLd() {
+    	
+        JsonLd jsonLd = new JsonLd();
+        jsonLd.setUseTypeCoercion(false);
+        jsonLd.setUseCuries(true);
+        jsonLd.addNamespacePrefix("http://www.w3.org/ns/oa-context-20130208.json", "oa");
+
+        JsonLdResource jsonLdResource = new JsonLdResource();
+        jsonLdResource.setSubject("");
+        jsonLdResource.addType("oa:Annotation");
+        jsonLdResource.putProperty("annotatedAt", "2012-11-10T09:08:07");
+        JsonLdProperty annotatedByProperty = new JsonLdProperty("annotatedBy");
+        JsonLdPropertyValue v1 = new JsonLdPropertyValue();
+        
+        v1.setType("http://xmlns.com/foaf/0.1/person");
+        v1.getValues().put("name", "annonymous web user");
+        annotatedByProperty.addValue(v1);        
+        
+        jsonLdResource.putProperty(annotatedByProperty);
+        
+        jsonLdResource.putProperty("serializedAt", "2012-11-10T09:08:07");
+        
+        JsonLdProperty serializedByProperty = new JsonLdProperty("serializedBy");
+        v1 = new JsonLdPropertyValue();
+        
+        v1.setType("prov:SoftwareAgent");
+        v1.getValues().put("name", "Annotorious");
+        v1.getValues().put("foaf:homepage", "http://annotorious.github.io/");
+        serializedByProperty.addValue(v1);        
+        
+        jsonLdResource.putProperty(serializedByProperty);
+        
+        jsonLdResource.putProperty("motivatedBy", "oa:tagging");
+
+        JsonLdProperty styledByProperty = new JsonLdProperty("styledBy");
+        v1 = new JsonLdPropertyValue();
+        
+        v1.setType("oa:CssStyle");
+        v1.getValues().put("hasSource", "http://annotorious.github.io/latest/themes/dark/annotorious-dark.css");
+        v1.getValues().put("styleClass", "annotorious-popup");
+        styledByProperty.addValue(v1);        
+        
+        jsonLdResource.putProperty(styledByProperty);
+        			
+        JsonLdProperty hasBodyProperty = new JsonLdProperty("hasBody");
+        v1 = new JsonLdPropertyValue();
+        
+        v1.addType("oa:Tag");
+        v1.addType("cnt:ContentAsText");
+        v1.addType("dctypes:Text");
+        v1.getValues().put("chars", "Vlad Tepes");
+        v1.getValues().put("dc:language", "ro");
+        v1.getValues().put("format", "text/plain");
+        hasBodyProperty.addValue(v1);        
+
+        JsonLdPropertyValue v2 = new JsonLdPropertyValue();
+        
+        v2.addType("oa:SemanticTag");
+        v2.getValues().put("foaf:page", "https://www.freebase.com/m/035br4");
+        hasBodyProperty.addValue(v2);        
+        
+        jsonLdResource.putProperty(hasBodyProperty);
+
+        JsonLdProperty hasTargetProperty = new JsonLdProperty("hasTarget");
+        v1 = new JsonLdPropertyValue();
+        
+        v1.addType("oa:SpecificResource");
+
+        JsonLdProperty hasSourceProperty = new JsonLdProperty("hasSource");
+        JsonLdPropertyValue v3 = new JsonLdPropertyValue();
+        
+        v3.setType("dctypes:Text");
+        v3.getValues().put("@id", "http://europeana.eu/portal/record//15502/GG_8285.html");
+        v3.getValues().put("format", "text/html");
+        hasSourceProperty.addValue(v3);        
+        v1.putProperty(hasSourceProperty);
+        
+        JsonLdProperty hasSelectorProperty = new JsonLdProperty("hasSelector");
+        JsonLdPropertyValue v4 = new JsonLdPropertyValue();
+        v4.setType(""); // if property is empty - set empty type
+        
+        hasSelectorProperty.addValue(v4);        
+        v1.putProperty(hasSelectorProperty);
+        
+        hasTargetProperty.addValue(v1);        
+        
+        jsonLdResource.putProperty(hasTargetProperty);
+
+        
+        jsonLd.put(jsonLdResource);
+
+        System.out.println("### jsonLdOriginal ###");
+        toConsole(jsonLd.toString());
+        
+        Gson gson = new Gson();
+        String jsonLdSerializedString = gson.toJson(jsonLd, JsonLd.class);
+        System.out.println("### jsonLdSerializedString ###");
+        toConsole(jsonLdSerializedString);
+        
+        JsonLd jsonLdDeserializedObject = gson.fromJson(jsonLdSerializedString, JsonLd.class);
+        String jsonLdDeserializedString = jsonLdDeserializedObject.toString();
+        System.out.println("### jsonLdDeserializedString ###");
+        toConsole(jsonLdDeserializedString);
+             
+        assertEquals(jsonLd.toString(), jsonLdDeserializedString);  
+        
+        System.out.println("### jsonLdDeserializedString with indent ###");
+        toConsole(jsonLdDeserializedObject.toString(4));
+    }
+        
     private void toConsole(String actual) {
         System.out.println(actual);
         String s = actual;
