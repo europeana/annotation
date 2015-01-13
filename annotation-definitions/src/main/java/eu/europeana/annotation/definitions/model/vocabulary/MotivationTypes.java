554ccd5dcd9f3e43c9d5a6576a50af7c601146cf
GrafR <GrafR@N3SIM1301.D03.arc.local>
2015-01-13 17:45:18 +0100

    issue #13: MotivationTypes extended by type constructor.

--- annotation-definitions/src/main/java/eu/europeana/annotation/definitions/model/vocabulary/MotivationTypes.java
+++ annotation-definitions/src/main/java/eu/europeana/annotation/definitions/model/vocabulary/MotivationTypes.java
@@ -2,5 +2,27 @@ package eu.europeana.annotation.definitions.model.vocabulary;
 
 public enum MotivationTypes {
 
-	BOOKMARKING, CLASSIFYING, COMMENTING, DESCRIBING, EDITING, HIGHLIGHTING, IDENTIFYING, LINKING, MODERATING, QUESTIONING, REPLYING, TAGGING 
+	  BOOKMARKING("oa:bookmarking")
+	, CLASSIFYING("oa:classifying")
+	, COMMENTING("oa:commenting")
+	, DESCRIBING("oa:describing")
+	, EDITING("oa:editing")
+	, HIGHLIGHTING("oa:highlighting")
+	, IDENTIFYING("oa:identifying")
+	, LINKING("oa:linking")
+	, MODERATING("oa:moderating")
+	, QUESTIONING("oa:questioning")
+	, REPLYING("oa:replying")
+	, TAGGING("oa:tagging"); 
+
+	
+	private String oaType;
+	
+	MotivationTypes(String oaType){
+		this.oaType = oaType;
+	}
+	
+	public String getOaType(){
+		return oaType;
+	}
 }
