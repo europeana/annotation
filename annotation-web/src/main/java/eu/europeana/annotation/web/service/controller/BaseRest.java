fbf1e9155e818ff0478720546e8fe6f94a9e2d02
gsergiu <sergiu.gordea@ait.ac.at>
2015-08-14 14:58:35 +0200

    #49 + correct representation of the error messages
    Change-Id: Ib8497c0cf3d3807623a0f791b165ed8ba18f8d4d

--- annotation-web/src/main/java/eu/europeana/annotation/web/service/controller/BaseRest.java
+++ annotation-web/src/main/java/eu/europeana/annotation/web/service/controller/BaseRest.java
@@ -1,11 +1,21 @@
 package eu.europeana.annotation.web.service.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import eu.europeana.annotation.definitions.model.Annotation;
@@ -18,8 +28,10 @@ import eu.europeana.annotation.definitions.model.utils.AnnotationBuilder;
 import eu.europeana.annotation.definitions.model.utils.AnnotationIdHelper;
 import eu.europeana.annotation.definitions.model.utils.TypeUtils;
 import eu.europeana.annotation.definitions.model.vocabulary.IdGenerationTypes;
+import eu.europeana.annotation.web.exception.HttpException;
 import eu.europeana.annotation.web.exception.authorization.UserAuthorizationException;
 import eu.europeana.annotation.web.exception.request.ParamValidationException;
+import eu.europeana.annotation.web.http.HttpHeaders;
 import eu.europeana.annotation.web.model.AnnotationOperationResponse;
 import eu.europeana.annotation.web.model.AnnotationSearchResults;
 import eu.europeana.annotation.web.model.ProviderSearchResults;
@@ -204,5 +216,47 @@ public class BaseRest {
 		//implement when authentication/authorization is available
 	}
 	
-	
+	@ExceptionHandler(HttpException.class)
+//	@ResponseBody
+	public ResponseEntity<String> handleHttpException(HttpException ex, HttpServletRequest req, HttpServletResponse response)
+			throws IOException {
+
+		ModelAndView res = getValidationReport(req.getParameter("wskey"), req.getServletPath(), null, ex);
+//		response.sendError(ex.getStatus().value());
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		//return res;
+		
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>(5);
+		headers.add(HttpHeaders.VARY, HttpHeaders.ACCEPT);
+		headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+		//headers.add(HttpHeaders.ETAG, "" + storedAnnotation.getLastUpdate().hashCode());
+		//headers.add(HttpHeaders.LINK, HttpHeaders.VALUE_LD_RESOURCE);
+
+		//TODO remove the usage of Model and View
+		ResponseEntity<String> responseEntity = new ResponseEntity<String>((String)res.getModel().get("json"), headers, ex.getStatus());
+		
+		return responseEntity;
+	}
+
+	@ExceptionHandler(RuntimeException.class)
+	//@ResponseBody
+	public ResponseEntity<String> handleException(Exception ex, HttpServletRequest req, HttpServletResponse response)
+			throws IOException {
+
+		ModelAndView res = getValidationReport(req.getParameter("wskey"), req.getServletPath(), null, ex);
+		//response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		//return res;
+		
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>(5);
+		headers.add(HttpHeaders.VARY, HttpHeaders.ACCEPT);
+		headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+		//headers.add(HttpHeaders.ETAG, "" + storedAnnotation.getLastUpdate().hashCode());
+		//headers.add(HttpHeaders.LINK, HttpHeaders.VALUE_LD_RESOURCE);
+
+		//TODO remove the usage of Model and View
+		ResponseEntity<String> responseEntity = new ResponseEntity<String>((String)res.getModel().get("json"), headers, HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		return responseEntity;
+	}
 }
