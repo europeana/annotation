d6f22283b072d968066aa790f728266e509e249e
gsergiu <sergiu.gordea@ait.ac.at>
2017-04-28 15:03:31 +0200

    Update ManagementRest.java

--- annotation-web/src/main/java/eu/europeana/annotation/web/service/controller/admin/ManagementRest.java
+++ annotation-web/src/main/java/eu/europeana/annotation/web/service/controller/admin/ManagementRest.java
@@ -1,9 +1,5 @@
 package eu.europeana.annotation.web.service.controller.admin;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
 import java.util.Date;
 import java.util.List;
 
