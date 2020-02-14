22545ca52521a07c25c192d3bd1aaa15621cd77c
gsergiu <sergiu.gordea@ait.ac.at>
2020-02-14 16:31:22 +0100

    #194 fixed unit test...
    Change-Id: Ib5c1d0ffc2673da1f88f4ed0f5f9986f0da190eb

--- annotation-client/src/test/java/eu/europeana/annotation/client/integration/webanno/transcribe/EdmTranscribingWithRightsTest.java
+++ annotation-client/src/test/java/eu/europeana/annotation/client/integration/webanno/transcribe/EdmTranscribingWithRightsTest.java
@@ -8,39 +8,38 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 
 import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpStatus;
 import org.apache.stanbol.commons.exception.JsonParseException;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
 
 import eu.europeana.annotation.definitions.model.Annotation;
 import eu.europeana.annotation.definitions.model.vocabulary.ResourceTypes;
 
 public class EdmTranscribingWithRightsTest extends BaseTranscribingTest {
 
-	@Test
-	public void createTranscriptionWithRights() throws IOException, JsonParseException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		
-		String requestBody = getJsonStringInput(TRANSCRIPTION_WITH_RIGHTS);
-		Annotation inputAnno = parseTranscription(requestBody);
-		
-		Annotation storedAnno = createTestAnnotation(TRANSCRIPTION_WITH_RIGHTS);
-		
-		//validate the reflection of input in output!
-		validateOutputAgainstInput(storedAnno, inputAnno);
-		
-		assertEquals(storedAnno.getBody().getInternalType(), ResourceTypes.SPECIFIC_RESOURCE.name());
-		assertTrue(storedAnno.getBody().getEdmRights().equals(inputAnno.getBody().getEdmRights()));		
-	}
-	
-	@Test
-	public void createTranscriptionWithoutRights() {
-		
-		// exception must be thrown
-		HttpException exception = assertThrows(HttpException.class, () -> {
-			createTestAnnotation(TRANSCRIPTION_WITHOUT_RIGHTS);
-		});
-		
-	    String actualMessage = exception.getMessage();
-	    String expectedMessage = "Missing mandatory field!";
-	    assertTrue(actualMessage.contains(expectedMessage));
-	}	
+    @Test
+    public void createTranscriptionWithRights() throws IOException, JsonParseException, IllegalAccessException,
+	    IllegalArgumentException, InvocationTargetException {
+
+	String requestBody = getJsonStringInput(TRANSCRIPTION_WITH_RIGHTS);
+	Annotation inputAnno = parseTranscription(requestBody);
+
+	Annotation storedAnno = createTestAnnotation(TRANSCRIPTION_WITH_RIGHTS);
+
+	// validate the reflection of input in output!
+	validateOutputAgainstInput(storedAnno, inputAnno);
+
+	assertEquals(storedAnno.getBody().getInternalType(), ResourceTypes.SPECIFIC_RESOURCE.name());
+	assertTrue(storedAnno.getBody().getEdmRights().equals(inputAnno.getBody().getEdmRights()));
+    }
+
+    @Test
+    public void createTranscriptionWithoutRights() throws IOException {
+
+	ResponseEntity<String> response = storeTestAnnotation(TRANSCRIPTION_WITHOUT_RIGHTS);
+	assertEquals(HttpStatus.SC_BAD_REQUEST, response.getStatusCodeValue());
+	String expectedMessage = "Missing mandatory field!";
+	assertTrue(response.getBody().contains(expectedMessage));
+    }
 }
