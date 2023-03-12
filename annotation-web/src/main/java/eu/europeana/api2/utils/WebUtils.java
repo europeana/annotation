58faba2b11d069ff698adbd0a22fe4e93992aadd
GordeaS <4517853+gsergiu@users.noreply.github.com>
2023-03-12 16:50:30 +0100

    addressing code smells #EA-3380

--- annotation-web/src/main/java/eu/europeana/api2/utils/WebUtils.java
+++ annotation-web/src/main/java/eu/europeana/api2/utils/WebUtils.java
@@ -13,40 +13,38 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  *
  */
 public class WebUtils {
-	
-	private static final String JSON_GENERATION_EXCEPTION = "Json Generation Exception: ";
-  private static final Logger log = LogManager.getLogger(WebUtils.class);
-	private static ObjectMapper objectMapper = new ObjectMapper();
-		
 
-	public static String toJson(Object object) {
-		return toJson(object, false, -1);
-	}
-		
-	public static String toJson(Object object, boolean shortObject, int objectId) {
-			
-		objectMapper.setSerializationInclusion(Include.NON_NULL);
-		String errorMessage = null;
-		try {
-			String jsonStr = objectMapper.writeValueAsString(object);	
-			if (shortObject) {
-				String idBeginStr = "id\":{";
-				int startIdPos = jsonStr.indexOf(idBeginStr);
-				int endIdPos = jsonStr.indexOf('}', startIdPos);
-				jsonStr = jsonStr.substring(0, startIdPos) + idBeginStr.substring(0, idBeginStr.length() - 1) 
-				    + Integer.valueOf(objectId) + jsonStr.substring(endIdPos + 1);
-			}
-			return jsonStr;
-		} catch (JsonGenerationException e) {
-			log.error(e);
-			errorMessage = JSON_GENERATION_EXCEPTION + e.getMessage();
-		} catch (JsonMappingException e) {
-			log.error(e);
-			errorMessage = JSON_GENERATION_EXCEPTION + e.getMessage();
-		} catch (IOException e) {
-			log.error(e);
-			errorMessage = "I/O Exception: " + e.getMessage();
-		}
-		return errorMessage;
-	}	
+
+  private static final Logger log = LogManager.getLogger(WebUtils.class);
+  private static ObjectMapper objectMapper = new ObjectMapper();
+
+
+  public static String toJson(Object object) {
+    return toJson(object, false, -1);
+  }
+
+  public static String toJson(Object object, boolean shortObject, int objectId) {
+
+    objectMapper.setSerializationInclusion(Include.NON_NULL);
+    String errorMessage = null;
+    try {
+      String jsonStr = objectMapper.writeValueAsString(object);
+      if (shortObject) {
+        String idBeginStr = "id\":{";
+        int startIdPos = jsonStr.indexOf(idBeginStr);
+        int endIdPos = jsonStr.indexOf('}', startIdPos);
+        jsonStr =
+            jsonStr.substring(0, startIdPos) + idBeginStr.substring(0, idBeginStr.length() - 1)
+                + Integer.valueOf(objectId) + jsonStr.substring(endIdPos + 1);
+      }
+      return jsonStr;
+    } catch (JsonGenerationException | JsonMappingException e) {
+      log.error(e);
+      errorMessage = "Json Generation Exception: " + e.getMessage();
+    } catch (IOException e) {
+      log.error(e);
+      errorMessage = "I/O Exception: " + e.getMessage();
+    }
+    return errorMessage;
+  }
 }
